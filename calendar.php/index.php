3a2528bc7e0078880363971bd928355867177967
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-03 09:02:10 +0900

    calendar.phpを作成

---
+++ calendar.php/index.php
@@ -0,0 +1,66 @@
+<?php
+
+$body = '';
+$period = new DatePeriod(
+  new DateTime('first day of this month'),
+  new DateInterval('P1D'),
+  new DateTime('first day of next month')
+);
+foreach ($period as $day) {
+  /* format()メソッドは DateTimeオブジェクトで使える書式*/
+  $body .= sprintf('<td>%d</td>', $day->format('d'));
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Calendar</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <body>
+      <table>
+        <thead>
+          <tr>
+            <th><a href="">&laquo;</a></th>
+            <th colspan="5">August 2015</th>
+            <th><a href="">&raquo;</a></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Sun</td>
+            <td>Mon</td>
+            <td>Tue</td>
+            <td>Wed</td>
+            <td>Thu</td>
+            <td>Fri</td>
+            <td>Sat</td>
+          </tr>
+          <tr>
+            <?php echo $body; ?>
+            <!-- <td class="youbi_0">1</td>
+            <td class="youbi_1">2</td>
+            <td class="youbi_2">3</td>
+            <td class="youbi_3">4</td>
+            <td class="youbi_4 today">5</td>
+            <td class="youbi_6">8<td> -->
+          <!-- </tr>
+          <tr>
+            <td class="youbi_0">30</td>
+            <td class="youbi_1">31</td>
+            <td class="gray">1</td>
+            <td class="gray">2</td>
+            <td class="gray">3</td>
+            <td class="gray">4</td>
+            <td class="gray">5</td> -->
+          </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th colspan="7"><a href="">Today</a></th>
+            </tr>
+          </tfoot>
+      </table>
+    </body>
+  </html>
