dd4db40e026279d9305dc6f4624d7729a88cb2d8
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-05 00:33:53 +0900

    Quiz.phpを作成

---
+++ quiz_php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Qiuz</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <div id="container">
+      <h1>Q. what is A?<h1>
+        <ul>
+          <li class="answer">A0</li>
+          <li class="answer">A1</li>
+          <li class="answer">A2</li>
+          <li class="answer">A3</li>
+        </ul>
+        <div id="btn" class="disable">Next Question>
+    </div>
+  </body>
+</html>
