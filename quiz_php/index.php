beaa3ba12070cd6b227d3b41bfe07f00c6660fe4
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-18 17:38:31 +0900

    Progate init

--- quiz_php/index.php
+++ quiz_php/index.php
@@ -5,9 +5,12 @@ require_once(__DIR__ . '/Quiz.php');
 
 $quiz = new Quiz();
 
-$data = $quiz->getCurrentQuiz();
-shuffle($data['a']);
+if (!$quiz->isFinished()) {
+  $data = $quiz->getCurrentQuiz();
+  shuffle($data['a']);
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -16,6 +19,17 @@ shuffle($data['a']);
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
+    <?php if ($quiz->isFinished()) : ?>
+    <div id="container">
+      <div id="result">
+        Your score
+        <div><?= h($quiz->getScore()); ?></div>
+      </div>
+      <a href=""><div id="btn">Replay?</div></a>
+    </div>
+    <a href="">finished!</a>
+    <?php $quiz->reset(); ?>
+    <?php else : ?>
     <div id="container">
       <h1>Q. <?= h($data['q']); ?></h1>
         <ul>
@@ -23,9 +37,10 @@ shuffle($data['a']);
             <li class="answer"><?= h($a); ?></li>
           <?php endforeach; ?>
         </ul>
-        <div id="btn" class="disabled">Next Question</div>
+        <div id="btn" class="disabled"><?= $quiz->isLast() ? 'Show Result' : 'Next Question' ; ?></div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/quiz.js"></script>
+    <?php endif; ?>
   </body>
 </html>
--- quiz_php/quiz.js
+++ quiz_php/quiz.js
@@ -3,13 +3,26 @@ $(function() {
 
   $('.answer').on('click', function() {
     var $selected = $(this);
+    if ($selected.hasClass('correct') || $selected.hasClass('wrong')) {
+      return;
+    }
     var answer = $selected.text();
 
     $.post('/_answer.php', {
     }).done(function(res) {
-      alert(res.correct_answer);
-      if (answer === res.correct_answer) {
-      } else {
+      $('.answer').each(function() {
+        if ($(this).text() === res.correct_answer) {
+          $(this).addClass('correct');
+        } else {
+          $(this).addClass('wrong');
+        }
+        $('#btn').removeClass('disabled');
+       });
+    })
+
+    $('#btn').on('click', function() {
+      if (!$(this).hasClass('disabled')) {
+        location.reload();
       }
     })
   });
