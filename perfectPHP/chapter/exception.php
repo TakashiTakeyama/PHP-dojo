114e78620e36b8dc7f44bb64a3437cd8f73e5684
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-12-27 21:49:22 +0900

    第五章途中

---
+++ perfectPHP/chapter/exception.php
@@ -0,0 +1,35 @@
+<?php
+
+function div ($v1, $v2)
+{
+  if ($v2 === 0) {
+    //Exceptionクラス
+    throw new Exception("arg #2 is zero.");
+  }
+  return $v1/$v2;
+}
+
+try {
+  echo div(1,2), PHP_EOL;
+  echo div(1,0), PHP_EOL;
+  echo div(2,1), PHP_EOL;
+} catch (ZeroDivisionExcption $e) {
+  echo 'Zero Division Exception!', PHP_EOL;
+  echo $e->getMessage(), PHP_EOL;
+} catch (Exception $e) {
+  echo $e->getMessage(), PHP_EOL;
+}
+
+set_error_handler(function ($errno, $errstr, $errfile, $errline)
+{
+  throw new ErrorException($errstr, 0 , $errno, $errfile, $errline);
+});
+
+try {
+  array_merge();
+} catch (ErrorException $e) {
+  echo 'Error ocuured!', PHP_EOL;
+  echo $e->getMessage(), PHP_EOL;
+  echo 'Stack Trace:', PHP_EOL;
+  echo $e->getTraceAsString();
+}
