8c511095b2f5457aabae2535b7bc105a85a1e181
Takashi Takeyama <chongzhizhushan74@gmail.com>
2020-01-11 18:00:19 +0900

    第9章終了

--- perfectPHP/sql_injection_03.php
+++ perfectPHP/sql_injection_03.php
@@ -5,9 +5,11 @@
 
  function check_user($dbh, $name, $pass) {
    $query = "SELECT * FROM users WHERE name = :name AND pass = :pass";
+   //prepare: 実行するステートメントを準備する。
    $sth = $dbh->prepare($query);
    $sth->bindParam(':name', $name, PDO::PARAM_STR);
    $sth->bindParam(':pass', $pass, PDO::PARAM_STR);
+   //execute(): プリペアドステートメントを実行する。
    $result = $sth->execute();
    if ($result !== false) {
      $rows = $sth->rowCount();
@@ -16,4 +18,17 @@
      }
    }
    return false;
- }
+  }
+
+if (strtolower($_SERVER['REQUEST_METHOD']) === 'post') {
+  $conn = pg_connect('host = localhost dbname=perfect '. 'user=user password=password');
+  $result = check_user($conn, $_POST['name'], $_POST['pass']);
+
+  if ($result === true) {
+    echo '<span style="color: #0000ff">ログインに成功しました</span>';
+  } else {
+    echo '<span style="color: #ff0000">ログインに失敗しました</span>';
+  }
+  pg_close($conn);
+  exit();
+}
---
+++ perfectPHP/sql_injection_04.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 入力されたユーザ名とパスワードを元に、ユーザが存在するか確認する。
+ */
+
+ function check_user($conn, $name, $pass) {
+   $name = pg_escape_string($name);
+   $pass = pg_escape_string($pass);
+   $query = "SELECT * FROM users WHERE name = '$name' AND pass = '$pass'";
+   $result = pg_query($conn, $query);
+   //クエリの戻り値がfalseでなければ、クエリ実行自体は正常
+   if ($result !== false) {
+     //取得したレコード数が1以上ならログイン成功
+      $rows = pg_num_rows($result);
+      if ($rows >= 1) {
+        return true;
+      }
+   }
+   return false;
+ }
